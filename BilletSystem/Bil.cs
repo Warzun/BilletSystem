e857be8d366466bdd6312a1c743e29ca5239aee4
Daniel Witt-Hansen <danielvwh@gmail.com>
2017-08-30 12:00:49 +0200

    Første commit af unit tests

---
+++ BilletSystem/Bil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystem
+{
+   public class Bil
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public int pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+    }
+    
+}
---
+++ BilletSystemTests/BilTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystem.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void prisTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void køretøjTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
